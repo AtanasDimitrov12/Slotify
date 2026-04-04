c2b97fbc73ebe89a665d396528f2fc7abc137ea3
Atanas Dimitrov <atanas@winstonpos.com>
2026-04-04 09:39:04 +0200

    Implement a separate DB for prod and tests

--- apps/api/api/src/app.module.ts
+++ apps/api/api/src/app.module.ts
@@ -25,7 +25,7 @@ import { TenantsModule } from './tenants/tenants.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: process.env.NODE_ENV === 'test' ? ['.env.test', '.env'] : ['.env'],
     }),
     MongooseModule.forRootAsync({
       inject: [ConfigService],
