1ce80d0ce47bd17a8b55004c46182ef3452836aa
Atanas Dimitrov <atanas@winstonpos.com>
2026-04-06 09:39:12 +0200

    Fix the main

--- apps/api/api/src/main.ts
+++ apps/api/api/src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(4000);
+  const port = process.env.PORT ? Number(process.env.PORT) : 4000;
+  await app.listen(port);
 }
 bootstrap();
